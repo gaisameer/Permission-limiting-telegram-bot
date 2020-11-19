f433c58af4fdef0be441c46c3fde2e99c7b5bf97
gaisameer <gaisameer@cet.ac.in>
2020-11-19 21:13:07 +0530

    mongodb running on atlas

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 
 
+//use this to run locally
+//mongodb://127.0.0.1:27017/telegram-bot
 
-mongoose.connect('mongodb://127.0.0.1:27017/telegram-bot',
+mongoose.connect('mongodb+srv://bots:botspass123@cluster0.r3ckb.mongodb.net/premium?retryWrites=true&w=majority',
 {   useNewUrlParser : true,
     useCreateIndex : true,
     useUnifiedTopology : true,
